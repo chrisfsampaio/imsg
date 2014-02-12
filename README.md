c2c9c35a5d7501aff17fc2ba78642597a20b7d6e
Christian Sampaio <christian.fsampaio@gmail.com>
2014-02-12 00:44:50 -0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-imsg
-====
+# imsg
 
-Chat using iMessage on your terminal!
+Tired of getting off your terminal screen to answer that dickheads friends of yours?  
+Now you can curse them right on the terminal!
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+    gem 'imsg'
+
+And then execute:
+
+    $ bundle
+
+Or install it yourself as:
+
+    $ gem install imsg
+
+## Usage
+
+imsg HELLLO FROM TERMINAL
+
+## Contributing
+
+1. Fork it ( http://github.com/chrisfsampaio/imsg/fork )
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+#<3
---
+++ lib/imsg.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+require "imsg/version"
+require 'appscript'
+
+def formatBuddies buddies
+   retVal = ""
+   count = 1
+   buddies.each do |buddy|
+      retVal += count.to_s + " - " +buddy.join(',') +"\n"
+      count += 1
+   end
+   retVal
+end
+
+def is_i str
+   !!(str =~ /^[-+]?[0-9]+$/)
+end
+
+str = ""
+ARGV.each do |value|
+  str +=value+" "
+end
+str = str.chomp(' ');
+ARGV.clear
+STDOUT.flush
+
+imsg = Appscript.app("Messages")
+a = imsg.chats.participants.get()
+b = []
+a.each do |v|
+  names = []
+  v.each do |buddy|
+      names.push buddy.name.get()
+  end
+  b.push names
+end
+puts "\n\nTo which chat you wanna send your message?"
+puts "(You can choose a number or type a buddy name/email)\n\n"
+puts formatBuddies b
+
+
+response = gets.chomp
+
+if is_i response
+  puts "Sending \'#{str}\'  to chat number #{response}"
+  `osascript -e 'tell application "Messages" to send \"#{str}\" to item #{response.to_i} of text chats'`
+else
+  puts "Sending \'#{str}\' to buddy \'#{response}\'"
+  `osascript -e 'tell application "Messages" to send \"#{str}\" to buddy \"#{response}\"'`
+end
