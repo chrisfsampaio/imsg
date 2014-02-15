52a7b715e86f7b9458de5bd53fd071062306ebc0
Christian Sampaio <christian.fsampaio@gmail.com>
2014-02-15 17:36:53 -0200

    Giving each class a file, thanks to @sarcilav on issue #20

--- lib/imsg.rb
+++ lib/imsg.rb
@@ -1,53 +1,9 @@
 #!/usr/bin/env ruby
-require "imsg/version"
-require 'appscript'
+require 'imsg/version'
+require 'model/chat'
 
 module ImsgHandler
-	CHAT_DISPLAY_LIMIT = 12
-
-	class Chat
-		attr_accessor :chat_number, :updated, :participants
-
-		def initialize(chat_number, updated, participants)
-			self.chat_number = chat_number
-			self.updated = updated
-			self.participants = participants
-		end
-
-		def to_s
-			"#{chat_number} - #{participants.map(&:name).join(", ")}"
-		end
-
-		def self.fetch_all
-			ascript_chats.map.with_index(1){ |chat, i| from_ascript_chat(chat, i) }
-		end
-
-		def self.ascript_chats
-			Appscript.app("Messages").chats.get()
-		end
-
-		def self.from_ascript_chat(chat, chat_number)
-			new(chat_number, chat.updated.get(), participants_from_ascript_chat(chat))
-		end
-
-		def self.participants_from_ascript_chat(chat)
-			chat.participants.get().map do |participant|
-				Participant.from_ascript_participant(participant)
-			end
-		end
-	end
-
-	class Participant
-		attr_accessor :name
-
-		def initialize(name)
-			self.name = name
-		end
-
-		def self.from_ascript_participant(ascript_participant)
-			self.new(ascript_participant.name.get())
-		end
-	end
+	CHAT_DISPLAY_LIMIT = 15
 
 	def self.display_chats(chats)
 		sort_by_updated(chats).first(CHAT_DISPLAY_LIMIT).map(&:to_s).join("\n")
